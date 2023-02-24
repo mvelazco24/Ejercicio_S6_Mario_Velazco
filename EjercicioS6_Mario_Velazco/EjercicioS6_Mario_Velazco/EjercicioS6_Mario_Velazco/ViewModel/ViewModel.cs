fb860bedaf6b4e13a9802c8cffa8971330d2114e
mvelazco24 <89883075+mvelazco24@users.noreply.github.com>
2023-02-23 19:26:02 -0600

    Commit con cambios

--- EjercicioS6_Mario_Velazco/EjercicioS6_Mario_Velazco/EjercicioS6_Mario_Velazco/ViewModel/ViewModel.cs
+++ EjercicioS6_Mario_Velazco/EjercicioS6_Mario_Velazco/EjercicioS6_Mario_Velazco/ViewModel/ViewModel.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 using EjercicioS6_Mario_Velazco.Model;
 
 namespace EjercicioS6_Mario_Velazco.ViewModel
 {
-    internal class ViewModel
+    public class ViewModelExpresion: INotifyiPropertyChanged 
     {
+            float num1;
+             float num2;
+        public float Num1
+        {
+            get => num1;
+            set
+            {
+                num1 = value;
+                var args = new PropertyChangedEventArgs(nameof(Num1));
+                PropertyChanged?.Invoke(this, args);
+            }
 
-    }
+
+        }
+        public float Num2
+        {
+            get => num2;
+            set
+            {
+                num2 = value;
+                var args = new PropertyChangedEventArgs(nameof(Num2));
+                PropertyChanged?.Invoke(this, args);
+            }
+
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+}      
+    
 }
+
